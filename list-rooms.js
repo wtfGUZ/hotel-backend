21759693b5490e9810409f191cff0afe7fd56086
Jakub <115201@g.elearn.uz.zgora.pl>
2026-03-05 18:11:02 +0100

    chore: clean up unused files and scripts

--- list-rooms.js
+++
@@ -1,3 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.room.findMany().then(r => console.log(JSON.stringify(r))).finally(() => prisma.$disconnect());
--- test.js
+++
@@ -1,3 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.room.findMany().then(r => console.log(r)).catch(e => console.error(e)).finally(() => prisma.$disconnect());
